a9d8880d0966bb5d93230c95ed7fcdd74316c010
MacQuy <macquydev77@gmail.com>
2025-03-19 14:54:50 +0700

    Add classes

---
+++ src/AuctionService/Entities/Auctions.cs
@@ -0,0 +1,14 @@
+﻿namespace AuctionService.Entites;
+
+public class Auctions
+{
+    public string? ID { get; set; }
+    public string? productID { get; set; }
+    public string? sellerID { get; set; }
+    public int startPrice { get; set; }
+    public int currentPrice { get; set; }
+    public int bidStep { get; set; }
+    public DateTime startTime { get; set; }
+    public DateTime endTime { get; set; }
+    public string winnerID { get; set; } = "";
+}
---
+++ src/AuctionService/Entities/Bids.cs
@@ -0,0 +1,13 @@
+﻿namespace AuctionService.Entites;
+
+public class Bids
+{
+    public string? ID { get; set; }
+    public string? userID { get; set; }
+    public int bidAmount { get; set; }
+    public DateTime bidTime { get; set; }
+
+    //Foreign key
+    public string? auctionID { get; set; }
+    public Auctions? auction { get; set; }
+}
