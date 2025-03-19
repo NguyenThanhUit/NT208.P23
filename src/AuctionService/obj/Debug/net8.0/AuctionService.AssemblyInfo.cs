35379a1805aa81f0727a723b6fe3b9cb168e8e60
MacQuy <macquydev77@gmail.com>
2025-03-19 14:38:29 +0700

    Simplify project

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9911bcd0322ef780e40af6724e38768b707f5ec1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfe67ceaebea7bd393827b773078d3f20e911226")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
