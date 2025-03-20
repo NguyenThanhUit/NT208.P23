e2aa9201c5c9320fd7de09616fb9f53e0c30a9c9
NguyenThanh <nguyenthanh91ndu1819@gmail.com>
2025-03-20 18:14:35 +0700

    new code

--- src/SearchService/obj/Debug/net8.0/SearchService.AssemblyInfo.cs
+++ src/SearchService/obj/Debug/net8.0/SearchService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37e2a90d3f841735a4f9bc96f6b6e08b2737ec64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fbe93d3835d68361b0c3398cd62a64051c09af0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
