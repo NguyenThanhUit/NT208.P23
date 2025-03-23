22c533f1fdfe93c0b9055b509765977e388dd958
nghphuc0112 <23521205@gm.uit.edu.vn>
2025-03-23 18:18:27 +0700

    Update ApplicationUser

--- src/IdentityService/Models/ApplicationUser.cs
+++ src/IdentityService/Models/ApplicationUser.cs
@@ -7,9 +7,15 @@ using Microsoft.AspNetCore.Identity;
 namespace IdentityService.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
+// ApplicationUser: Dùng để lưu trữ thông tin người dùng thực tế trong CSDL
 public class ApplicationUser : IdentityUser
 {
-    // public string? Address {get; set;}
-    // public DateTime CreateAt {get; set;} = DateTime.UtcNow;
-    // public DateTime UpdatedAt {get; set;}
+    public string? FullName {get; set;}
+    public string? Address {get; set;}
+    public DateTime CreateAt {get; set;} = DateTime.UtcNow;
+    public DateTime UpdatedAt {get; set;} = DateTime.UtcNow;
 }
+
+// Update Database
+// Tạo migration: dotnet ef migrations add AddFullNameAndAddress
+// Áp dụng Migration vào CSDL: dotnet ef database update
